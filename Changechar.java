83fa8e546ab6f3f7298ad5613b7bb189ddb4ec20
handsomemanNO76 <103016443+handsomemanNO76@users.noreply.github.com>
2022-04-16 15:49:51 +0800

    Add files via upload

---
+++ Changechar.java
@@ -0,0 +1,5 @@
+public class Changechar{
+	public static void main(String[] args){
+		System.out.println("I said :\"I love you\"");
+	}
+}
---
+++ Homework02.java
@@ -0,0 +1,11 @@
+public class Homework02{
+	public static void main(String[] args){
+		char a1 = '\n';
+		char a2 = '\\';
+		char a3 = '\t';
+		char a4 = '\r';
+		char a5 = '1';
+		char a6 = '2';
+		System.out.println(a1+"\n"+a2+"\n"+a3+"\n"+a4+"\n"+a5+"\n"+a6);
+	}
+}
